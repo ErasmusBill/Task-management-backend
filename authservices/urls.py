40a0db258f37a6b005120ff9f96654d5096cd11f
erasmus <erasmuschawey12345@gmail.com>
2025-02-26 21:41:48 +0000

    made changes to update-profile endpoint

--- authservices/urls.py
+++ authservices/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('login/', views.UserLogin.as_view(), name='user_login'),
     path('logout/', views.Logout.as_view(), name='user_logout'),
     path('change-password/', views.ChangePasswordView.as_view(), name='change_password'),
-    path('update-profile/<int:pk>/', views.UpdateProfileView.as_view(), name='user_update'),
+    path('update-profile/', views.UpdateProfileView.as_view(), name='user_update'),
     path('list-users/', views.UserListView.as_view(), name='user_list'),
 ]
