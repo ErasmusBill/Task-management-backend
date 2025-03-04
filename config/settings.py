32d8a3e67696add012d500c5b20cf17b9362e90d
erasmus <erasmuschawey12345@gmail.com>
2025-03-04 02:05:59 +0000

    made config to email settings

--- config/settings.py
+++ config/settings.py
@@ -170,9 +170,9 @@ SIMPLE_JWT = {
 }
 
 #Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_PORT = 465 
-EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = 'erasmuschawey12345@gmail.com'  
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # You're missing ".EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'erasmuschawey12345@gmail.com'
 EMAIL_HOST_PASSWORD = 'hynw qyme vyqy vumk'  
