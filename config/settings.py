eae71bb1ce608b1ab262c7f25530ab735c096c77
erasmus <erasmuschawey12345@gmail.com>
2025-03-10 13:09:17 +0000

    made changes to email url

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "https://kf7-passionate-hubble.circumeo-apps.net",
     "https://task-management-backend-production-3436.up.railway.app",
-    "https://task-management-gold-iota.vercel.app/"
+    "https://task-management-gold-iota.vercel.app",
 ]
 
 FRONTEND_URL = "https://task-management-gold-iota.vercel.app/"
