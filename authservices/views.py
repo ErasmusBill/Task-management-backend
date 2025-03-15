ee63bcf5ba46ab25f9e86468da14c48a1036ccab
erasmus <erasmuschawey12345@gmail.com>
2025-03-15 18:22:58 +0000

    made changes to the api

--- authservices/views.py
+++ authservices/views.py
@@ -14,7 +14,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from authservices.email_verification import verify_email
+from .email_verification import verify_email
 
 
 User = get_user_model()
